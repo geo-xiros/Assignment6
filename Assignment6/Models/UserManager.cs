cf9302e87477bd8f667e2fc09ad64898e20abe49
geo.xiros <geo.xiros@gmailcom>
2019-02-19 16:48:14 +0200

    Add UserManager

---
+++ Assignment6/Models/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Assignment6.Models
+{
+    public class UserManager
+    {
+        ApplicationDbContext _db;
+        public UserManager(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public User Login(string username, string password)
+        {
+            return _db.User.FirstOrDefault(u => u.Username == username && u.Password == password);
+        }
+
+        public bool UserExists(string username)
+        {
+            return _db.User.Any(u => u.Username == username);
+        }
+    }
+}
