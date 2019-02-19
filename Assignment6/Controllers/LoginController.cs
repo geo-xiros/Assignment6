32b9ed6cd22131f6af886611e32b65d914fa73b6
geo.xiros <geo.xiros@gmailcom>
2019-02-19 16:58:28 +0200

    add login, success views with login controller

---
+++ Assignment6/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Assignment6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment6.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            UserManager manager = new UserManager(_db);
+            var loggedInUser = manager.Login(user.Username, user.Password);
+
+            if (loggedInUser != null)
+            {
+                Session["user"] = loggedInUser;
+                return View("Success", loggedInUser);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
