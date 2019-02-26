398d97b3284130c02bf63cf3f2541987fbc016d4
geo.xiros <geo.xiros@gmailcom>
2019-02-26 16:56:31 +0200

    some more refactoring for Pending documents...

---
+++ Assignment6/Factories/PendingDocuments.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Assignment6.Models;
+
+namespace Assignment6.Factories
+{
+
+    public abstract class PendingDocuments
+    {
+        protected ApplicationDbContext _db;
+        protected int _userId;
+        protected int _role;
+        protected int _maxCountOfDocumentsForCreteria;
+
+        public PendingDocuments(ApplicationDbContext db, int userId, Roles role, int maxCountOfDocumentsForCreteria)
+        {
+            _db = db;
+            _userId = userId;
+            _role = (int)role;
+            _maxCountOfDocumentsForCreteria = maxCountOfDocumentsForCreteria;
+        }
+
+        public IEnumerable<Document> GetDocuments()
+        {
+            return _db.Documents
+                .Get()
+                .Where(d =>
+                    d.IsCompletedByRole.Where(CompletedDocuments).Count() == _maxCountOfDocumentsForCreteria ||
+                    d.AssignedDocuments.Any(PendingOwnedTasks));
+        }
+
+        protected abstract bool CompletedDocuments(KeyValuePair<int, int> task);
+
+        private bool PendingOwnedTasks(DocumentAssign documentAssign)
+        {
+            return documentAssign.AssignedToRoleId == _role &&
+                documentAssign.PurchasedByUserId == _userId &&
+                documentAssign.Status == "Pending";
+        }
+    }
+
+}
---
+++ Assignment6/Factories/TesterPendingDocuments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Assignment6.Models;
+
+namespace Assignment6.Factories
+{
+
+    public class TesterPendingDocuments : PendingDocuments
+    {
+        public TesterPendingDocuments(ApplicationDbContext db, int userId, Roles role) : base(db, userId, role, 4) { }
+
+        protected override bool CompletedDocuments(KeyValuePair<int, int> task)
+        {
+            return (task.Key == (int)Roles.Analyst && (task.Value == 1 || task.Value == 0)) ||
+                   (task.Key == (int)Roles.Architect && (task.Value == 1 || task.Value == 0)) ||
+                   (task.Key == (int)Roles.Programmer && (task.Value == 1)) ||
+                   (task.Key == (int)Roles.Tester && task.Value == 0);
+        }
+
+    }
+
+}
