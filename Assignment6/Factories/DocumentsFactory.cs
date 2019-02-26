0e1f79e4b5e45e9049f52e3259828099dfb330a8
geo.xiros <geo.xiros@gmailcom>
2019-02-26 11:27:02 +0200

    PendingDocuments more refactoring

--- Assignment6/Factories/DocumentsFactory.cs
+++ Assignment6/Factories/DocumentsFactory.cs
@@ -15,10 +15,10 @@ namespace Assignment6.Factories
             ApplicationDbContext db = new ApplicationDbContext();
             pendingDocumentsByRole = new Dictionary<string, PendingDocuments>();
 
-            pendingDocumentsByRole.Add(Roles.Analyst.ToString(), new AnalystPendingDocuments(db, userId));
-            pendingDocumentsByRole.Add(Roles.Architect.ToString(), new ArchitectPendingDocuments(db, userId));
-            pendingDocumentsByRole.Add(Roles.Programmer.ToString(), new ProgrammerPendingDocuments(db, userId));
-            pendingDocumentsByRole.Add(Roles.Tester.ToString(), new TesterPendingDocuments(db, userId));
+            pendingDocumentsByRole.Add(Roles.Analyst.ToString(), new AnalystPendingDocuments(db, userId,Roles.Analyst));
+            pendingDocumentsByRole.Add(Roles.Architect.ToString(), new ArchitectPendingDocuments(db, userId, Roles.Architect));
+            pendingDocumentsByRole.Add(Roles.Programmer.ToString(), new ProgrammerPendingDocuments(db, userId, Roles.Programmer));
+            pendingDocumentsByRole.Add(Roles.Tester.ToString(), new TesterPendingDocuments(db, userId, Roles.Tester));
 
         }
         public PendingDocuments this[string role]
@@ -33,171 +33,111 @@ namespace Assignment6.Factories
     {
         protected ApplicationDbContext _db;
         protected int _userId;
+        protected int _role;
         public abstract IEnumerable<Document> GetDocuments();
-    }
-
-    public class AnalystPendingDocuments : PendingDocuments
-    {
-        public AnalystPendingDocuments(ApplicationDbContext db, int userId)
+        protected bool PendingOwnedTasks(DocumentAssign documentAssign)
         {
-            _db = db;
-            _userId = userId;
-        }
-        public override IEnumerable<Document> GetDocuments()
-        {
-            return _db.Documents
-                .Get()
-                .Where(d => d.AssignedDocuments.Any(PendingOwnedDocument));
-        }
-        private bool PendingOwnedDocument(DocumentAssign documentAssign)
-        {
-            return documentAssign.AssignedToRoleId == (int)Roles.Analyst && documentAssign.Status == "Pending" && documentAssign.PurchasedByUserId == _userId;
-        }
-    }
-
-    public class ArchitectPendingDocuments : PendingDocuments
-    {
-        public ArchitectPendingDocuments(ApplicationDbContext db, int userId)
-        {
-            _db = db;
-            _userId = userId;
-        }
-        public override IEnumerable<Document> GetDocuments()
-        {
-            var x = _db.Documents
-                .Get()
-                .Where(d =>
-                    d.IsCompletedByRole.Where(CompletedAnalystTasks).Count() == 2 ||
-                    d.AssignedDocuments.Any(PendingOwnedDocument));
-            return x;
-        }
-        private static bool CompletedAnalystTasks(KeyValuePair<int, bool> task)
-        {
-            return (task.Key == (int)Roles.Analyst && task.Value == true) ||
-                   (task.Key == (int)Roles.Architect && task.Value == false);
-        }
-        private bool PendingOwnedDocument(DocumentAssign documentAssign)
-        {
-            return documentAssign.AssignedToRoleId == (int)Roles.Architect &&
+            return documentAssign.AssignedToRoleId == _role &&
                 documentAssign.PurchasedByUserId == _userId &&
                 documentAssign.Status == "Pending";
         }
     }
 
-    public class ProgrammerPendingDocuments : PendingDocuments
+    public class AnalystPendingDocuments : PendingDocuments
     {
-        public ProgrammerPendingDocuments(ApplicationDbContext db, int userId)
+        public AnalystPendingDocuments(ApplicationDbContext db, int userId, Roles role)
         {
             _db = db;
             _userId = userId;
+            _role = (int)role;
         }
         public override IEnumerable<Document> GetDocuments()
         {
-            var result = _db.Documents
+            return _db.Documents
+                .Get()
+                .Where(d => 
+                    d.IsCompletedByRole.Where(CompletedTasks).Count() == 1 || 
+                    d.AssignedDocuments.Any(PendingOwnedTasks));
+        }
+        private bool CompletedTasks(KeyValuePair<int, bool> task)
+        {
+            return (task.Key == 0);
+        }
+    }
+
+    public class ArchitectPendingDocuments : PendingDocuments
+    {
+        public ArchitectPendingDocuments(ApplicationDbContext db, int userId, Roles role)
+        {
+            _db = db;
+            _userId = userId;
+            _role = (int)role;
+        }
+        public override IEnumerable<Document> GetDocuments()
+        {
+            return _db.Documents
                 .Get()
                 .Where(d =>
-                    d.AssignedDocuments.Any(a =>
-                         (a.AssignedToRoleId == (int)Roles.Architect && a.Status == "Completed") ||
-                         (a.AssignedToRoleId == (int)Roles.Programmer && a.Status == "Pending" && a.PurchasedByUserId == _userId))
-                    ).Distinct()
-                .ToList();
-            result.RemoveAll(d => d.AssignedDocuments.Any(a =>
-                (a.AssignedToRoleId == (int)Roles.Programmer && a.Status == "Completed") ||
-                (a.AssignedToRoleId == (int)Roles.Analyst && a.Status == "Pending")));
-
-            return result;
+                    d.IsCompletedByRole.Where(CompletedTasks).Count() == 2 ||
+                    d.AssignedDocuments.Any(PendingOwnedTasks));
         }
+        private bool CompletedTasks(KeyValuePair<int, bool> task)
+        {
+            return (task.Key == (int)Roles.Analyst && task.Value == true) ||
+                   (task.Key == (int)Roles.Architect && task.Value == false);
+        }
+
+    }
+
+    public class ProgrammerPendingDocuments : PendingDocuments
+    {
+        public ProgrammerPendingDocuments(ApplicationDbContext db, int userId, Roles role)
+        {
+            _db = db;
+            _userId = userId;
+            _role = (int)role;
+        }
+        public override IEnumerable<Document> GetDocuments()
+        {
+            return _db.Documents
+                .Get()
+                .Where(d =>
+                    d.IsCompletedByRole.Where(CompletedTasks).Count() == 3 ||
+                    d.AssignedDocuments.Any(PendingOwnedTasks));
+        }
+        private  bool CompletedTasks(KeyValuePair<int, bool> task)
+        {
+            return (task.Key == (int)Roles.Analyst && task.Value == true) ||
+                   (task.Key == (int)Roles.Architect && task.Value == true) ||
+                   (task.Key == (int)Roles.Programmer && task.Value == false);
+        }
+
     }
 
     public class TesterPendingDocuments : PendingDocuments
     {
-        public TesterPendingDocuments(ApplicationDbContext db, int userId)
+        public TesterPendingDocuments(ApplicationDbContext db, int userId, Roles role)
         {
             _db = db;
             _userId = userId;
+            _role = (int)role;
         }
         public override IEnumerable<Document> GetDocuments()
         {
-            var result = _db.Documents
+            return _db.Documents
                 .Get()
                 .Where(d =>
-                    d.AssignedDocuments.Any(a =>
-                         (a.AssignedToRoleId == (int)Roles.Programmer && a.Status == "Completed") ||
-                         (a.AssignedToRoleId == (int)Roles.Tester && a.Status == "Pending" && a.PurchasedByUserId == _userId))
-                    ).Distinct()
-                .ToList();
-            result.RemoveAll(d => d.AssignedDocuments.Any(a =>
-                (a.AssignedToRoleId == (int)Roles.Tester && a.Status == "Completed") ||
-                (a.AssignedToRoleId == (int)Roles.Analyst && a.Status == "Pending") ||
-                (a.AssignedToRoleId == (int)Roles.Architect && a.Status == "Pending")));
-
-            return result;
-
+                    d.IsCompletedByRole.Where(CompletedTasks).Count() == 4 ||
+                    d.AssignedDocuments.Any(PendingOwnedTasks));
         }
+        private bool CompletedTasks(KeyValuePair<int, bool> task)
+        {
+            return (task.Key == (int)Roles.Analyst && task.Value == true) ||
+                   (task.Key == (int)Roles.Architect && task.Value == true) ||
+                   (task.Key == (int)Roles.Programmer && task.Value == true) ||
+                   (task.Key == (int)Roles.Tester && task.Value == false);
+        }
+
     }
 
-
 }
-
-
-//static void Main(string[] args)
-//{
-//    List<Doc> docs = new List<Doc>()
-//            {
-//                new Doc(){
-//                    id = 1,
-//                    completedTasks = new Dictionary<int, bool>()
-//                    {
-//                        { 1, true },
-//                        { 2, true },
-//                        { 3, true },
-//                        { 4, false }
-//                    }
-//                },
-//                new Doc(){
-//                    id = 2,
-//                    completedTasks = new Dictionary<int, bool>()
-//                    {
-//                        { 1, false },
-//                        { 2, true },
-//                        { 3, true },
-//                        { 4, true }
-//                    }
-//                }
-
-//            };
-
-//    var x1 = docs.Where(d => d.completedTasks.Where(SecondRoleTasks).Count() == 2);
-//    var x2 = docs.Where(d => d.completedTasks.Where(ThirdRoleTasks).Count() == 3);
-//    var x3 = docs.Where(d => d.completedTasks.Where(FourthRoleTasks).Count() == 3);
-
-//    Console.WriteLine($"x1={x1.FirstOrDefault()?.id}, count={x1.Count()}");
-//    Console.WriteLine($"x2={x2.FirstOrDefault()?.id}, count={x2.Count()}");
-//    Console.WriteLine($"x3={x3.FirstOrDefault()?.id}, count={x3.Count()}");
-
-//    Console.ReadKey();
-//}
-//public class Doc
-//{
-//    public int id { get; set; }
-//    public Dictionary<int, bool> completedTasks;
-//}
-
-//public static bool SecondRoleTasks(KeyValuePair<int, bool> role)
-//{
-//    return (role.Key == 1 && role.Value == true) ||
-//           (role.Key == 2 && role.Value == false);
-//}
-//public static bool ThirdRoleTasks(KeyValuePair<int, bool> role)
-//{
-//    return (role.Key == 1 && role.Value == true) ||
-//           (role.Key == 2 && role.Value == true) ||
-//           (role.Key == 3 && role.Value == false);
-//}
-//public static bool FourthRoleTasks(KeyValuePair<int, bool> role)
-//{
-//    return (role.Key == 1 && role.Value == true) ||
-//           (role.Key == 2 && role.Value == true) ||
-//           (role.Key == 3 && role.Value == true) ||
-//           (role.Key == 4 && role.Value == false);
-//}
